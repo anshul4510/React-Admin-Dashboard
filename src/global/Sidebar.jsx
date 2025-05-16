6485f6accd8440163a64cc3d58e60138afdc8fd2
anshul_1905 <anshul4510@users.noreply.github.com>
2025-05-16 15:07:53 +0530

    imageSidebar

--- src/global/Sidebar.jsx
+++ src/global/Sidebar.jsx
@@ -16,6 +16,7 @@ import PieChartOutlineOutlinedIcon from "@mui/icons-material/PieChartOutlineOutl
 import TimelineOutlinedIcon from "@mui/icons-material/TimelineOutlined";
 import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
 import MapOutlinedIcon from "@mui/icons-material/MapOutlined";
+import user from "REACT/DashboardReact/src/assets/user.png";
 const Item = ({ title, to, icon, selected, setSelected }) => {
     const theme = useTheme();
     const colors = tokens(theme.palette.mode);
@@ -70,7 +71,7 @@ const Sidebar = () => {
                     {!isCollapsed && (
                         <Box mb="25px">
                             <Box display="flex" justifyContent="center" alignItems="center">
-                                <img src={`\\src\\assets\\user.png`} alt="profile-user" width="100px" height="100px" style={{ cursor: "pointer", borderRadius: "50%" }} />
+                                <img src={user} alt="profile-user" width="100px" height="100px" style={{ cursor: "pointer", borderRadius: "50%" }} />
                             </Box>
                             <Box textAlign="center">
                                 <Typography variant="h2" color={colors.grey[100]} fontWeight="bold" sx={{ m: "10px 0 0 0 " }}>Anshul Patel</Typography>
